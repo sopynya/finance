2b4a120cf387ebf4a284d5d676f5a5f1df0ca46b
soso <164677008+sopynya@users.noreply.github.com>
2026-03-30 15:39:05 -0300

    middleware

---
+++ src/middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const token = req.cookies.get("token")?.value;
+
+  const { pathname } = req.nextUrl;
+
+  const publicRoutes = ["/login", "/register"];
+
+  const isPublic = publicRoutes.includes(pathname);
+
+  if (!token && !isPublic) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (token && isPublic) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/overview/:path*", "/budgets/:path*", "/transactions/:path*","/pots/:path*", "/bills/:path*"],
+};
